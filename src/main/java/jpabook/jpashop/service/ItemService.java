773b4c6064a9c3dc0370793680c9ba38125240ac
dsadara <chaehyun.forest@gmail.com>
2023-07-19 04:00:46 +0900

    변경 감지를 통한 상품 업데이트

--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.service;
 
+import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -19,6 +20,15 @@ public class ItemService {
         itemRepository.save(item);
 
         return item.getId();
+    }
+
+    @Transactional
+    public void updateItem(Long itemId, String name, int price, int stockQuantity) {
+        Item findItem = itemRepository.findOne(itemId);     // 영속성 컨텍스트가 관리하는 객체를 꺼내옴
+        findItem.setName(name);
+        findItem.setPrice(price);
+        findItem.setStockQuantity(stockQuantity);
+        // itemRepository.save()를 하지 않아도 트랜젝션 어노테이션이 있으니 flush 되면서 업데이트가 됨
     }
 
     public List<Item> findItems() {
